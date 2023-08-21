44a55866062a61ffe8d3f2569aaecb83c0589bff
silambarasan3101 <139097561+silambarasan3101@users.noreply.github.com>
2023-08-21 17:30:56 +0530

    SQLQuery 1.11.sql

--- SQLQuery 1.1.sql
+++ SQLQuery 1.1.sql
@@ -4,7 +4,7 @@ use Assissted
 Create Table School
 (Id int Primary Key,
 Name nvarchar(50),
-Class nvarchar(50))
+Class nvarchar(50)) 
 
 Insert Into School Values (1,'Praveen','12B');
 Insert Into School Values(2,'Sanjay','12C');
